365816f86532130e852c19308ec5a96a040a7cb0
Pradyumn <pradyumnkrshukla.4672@gmail.com>
2026-08-22 17:01:27 +0530

    refactor: remove duplicate installer logging

--- bootstrap/installers/environment.sh
+++ bootstrap/installers/environment.sh
@@ -6,21 +6,20 @@ install_environment() {
 
     print_info "Installing Nocturne environment..."
 
-    print_info "Preparing package manager..."
     install_apt
 
-    print_info "Installing development tools..."
+    
     install_git
     install_python
     install_node
 
-    print_info "Installing shell environment..."
+    
     install_shell
 
-    print_info "Installing Starship..."
+    
     install_starship
 
-    print_info "Installing SSH banner..."
+    
     install_banner
 
     print_success "Nocturne environment installed successfully."
